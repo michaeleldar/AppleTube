0bf7c76cd42250877a5d6cf424bc5fc384643ebd
Michael Halpin <halpinm2010@gmail.com>
2022-03-16 08:19:53 +0000

    .

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from fileinput import filename
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
-def hello():
+def index():
+    url_for('static', filename="style.css")
     return render_template('index.html')
+
